cabef837cce515c19dfb173ab8c5014fac263e58
Walisson Santos <walissonsantost008@gmail.com>
2025-04-25 20:00:49 -0300

    Genéricos delimitados

---
+++ untitled/src/genericosdelimitados/aplication/Program.java
@@ -0,0 +1,35 @@
+package genericosdelimitados.aplication;
+
+import genericosdelimitados.entities.Product;
+import genericosdelimitados.services.CalculationService;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+    public static void main(String[] args){
+
+        List<Product> list = new ArrayList<>();
+
+        String path = "D:\\exercicio\\interfaces\\Generics-Set-Map\\untitled\\src\\in.txt";
+
+        try (BufferedReader bf = new BufferedReader(new FileReader(path))){
+            String line = bf.readLine();
+            while (line != null){
+                String[] fields = line.split(",");
+                list.add(new Product(fields[0], Double.parseDouble(fields[1])));
+                line = bf.readLine();
+            }
+
+            Integer x = CalculationService.max(list);
+            System.out.println("Max:");
+            System.out.println(x);
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+}
