1c2120fc9f6187253289a120f9d68cbe632dc700
Walisson Santos <walissonsantost008@gmail.com>
2025-04-26 14:31:06 -0300

    HashCode e Equals

---
+++ untitled/src/hashCodeeequals/aplication/Program.java
@@ -0,0 +1,15 @@
+package hashCodeeequals.aplication;
+
+import hashCodeeequals.entities.Client;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Client c1 = new Client("Maria", "maria@gmail.com");
+        Client c2 = new Client("Alex", "alex@gmail.com");
+
+        System.out.println(c1.hashCode());
+        System.out.println(c2.hashCode());
+        System.out.println(c1.equals(c2));
+    }
+}
